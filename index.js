188d2033b800ce4617929a717583aede394e1e57
Sophieriis <119849850+Sophieriis@users.noreply.github.com>
2023-02-05 21:51:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// function message (){
+//     var Name = document.getElementById('name')
+//     var msg = document.getElementById('msg')
+//     const succes = document.getElementById('succes')
+//     const danger = document.getElementById('danger')
+//     if(Name.value === '' || msg.value === '' === ''){
+//         danger.style.display = 'block';
+//     }
+//     else{
+//         setTimeout(() => {
+//         Name.value = '';
+//         msg.value = '';
+//         }, 2000);
+        
+//         succes.style.display = 'block'
+//     }
+// }
